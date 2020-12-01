8e01dc1bfeb96e5b2addb46522c127fbc8465dd6
Miguel Hernandez Liebano <mhliebano@gmail.com>
2020-11-30 22:46:05 -0400

    cambios para pruebas

--- main.dart
+++ main.dart
@@ -8,19 +8,24 @@ void main() async {
   w.setDimension(400, 300);
   Renderer r = Renderer(sdl.sdllib);
 
+  Surface s = Surface.SDL_CreateRGBSurface(400, 300, 0, 0, 0, 0, sdl.sdllib);
+  print(s.refcount);
+  //s.SDL_CreateRGBSurface(0, 400, 300, 0, 0, 0, 255);
+
   sdl.SDL_Init();
-  w.SDL_CreateWindow("Miwindow", 0);
+  w.SDL_CreateWindow("Mi Window", 0);
   r.SDL_CreateRenderer(w.window);
-  r.SDL_SetRenderDrawColor(255, 0, 0, 0);
+
   // await Future.delayed(const Duration(seconds: 5), () => null);
   // w.SDL_SetWindowSize(800, 600);
   // w.SDL_SetWindowTitle("Mas Grande");
   DateTime start = DateTime.now();
   while (run) {
-    if (DateTime.now().difference(start).inSeconds == 10) {
+    if (DateTime.now().difference(start).inSeconds == 2) {
       print("fin");
       run = false;
     }
+    r.SDL_SetRenderDrawColor(255, 0, 0, 0);
   }
   r.SDL_DestroyRenderer();
   w.SDL_DestroyWindow();
