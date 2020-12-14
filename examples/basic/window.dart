f853a20f4909edb09714933a47165d334aac8662
Miguel Hernandez Liebano <mhliebano@gmail.com>
2020-12-13 20:50:18 -0400

    pruebas

--- examples/basic/window.dart
+++ examples/basic/window.dart
@@ -6,31 +6,21 @@ void main() async {
 
   Window window = Window();
   window.setDimension(800, 600); //Seteo parametros de la ventana
-  window.setWindowFlag = Window.SDL_WINDOW_FLAG;
+  window.setWindowFlag = Window.SDL_WINDOW_OPENGL;
 
   dartSDL.SDL_Init(); //Se inicializa el SDL
-  window.CreateWindow("Ventana SDL"); //Creo la Ventana
-  print(window.GetWindowId());
-  sleep(Duration(seconds: 5));
+  window.CreateWindow("Ventana SDL 1"); //Creo la Ventana
 
-  print(window.GetWindowFlags());
-  print(window.GetWindowPosition());
+  GL_Context gl1 = window.GL_CreateContext();
+  print(gl1.idContext);
 
-  // window.SDL_SetWindowData("testdatapointer", 1234);
-  // print(window.SDL_GetWindowData());
+  print(window.GL_GetDrawableSize());
 
-  //print(window.SDL_GetWindowDisplayIndex());
-  //DisplayMode dm = window.SDL_GetWindowDisplayMode();
-  //print(dm.width);
-  DisplayMode initDM = window.GetWindowDisplayMode();
-  print("initDM ${initDM.format},${initDM.width}");
-  sleep(Duration(seconds: 5));
-  Display vdo = Display();
-  DisplayMode displayMode = vdo.GetDisplayMode(0, 40);
-  window.SetWindowDisplayMode(displayMode);
-  initDM = window.GetWindowDisplayMode();
-  print("newDM ${initDM.format} ${initDM.width}");
-  sleep(Duration(seconds: 5));
+  sleep(Duration(seconds: 3));
+
+  window.GL_DeleteContext(gl1);
+
   window.DestroyWindow(); //Elimino la ventana
+
   dartSDL.SDL_Quit(); //Salgo del Programa
 }
